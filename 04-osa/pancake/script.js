6b785032128d1e8c33250ebfe4520ba22ebebe4e
Misteri02 <julius.tuominen02@gmail.com>
2024-04-26 09:37:27 +0300

    osa 4 valmis

--- 04-osa/pancake/script.js
+++ 04-osa/pancake/script.js
@@ -1,6 +1,32 @@
-//price id totalPrice
-//select id type
+const form = document.querySelector('.form-container');
 
-const type = document.querySelector("type");
-const totalPrice = document.getElementById("totalPrice");
+const pancakePriceCalc = () => {
+  const typeSelect = document.querySelector('#type');
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  const totalPriceElement = document.querySelector('#totalPrice');
+  const priceBanner = document.querySelector('.price-banner');
 
+  let total = parseInt(typeSelect.value);
+
+  for (const item of checkboxes) {
+    if (item.checked) {
+      total += parseInt(item.value);
+    }
+  }
+
+  totalPriceElement.textContent = `$${total}`;
+
+  priceBanner.animate(
+    [
+      { transform: 'scale(1)' },
+      { transform: 'scale(1.05)' },
+      { transform: ' scale(1)' },
+    ],
+    {
+      duration: 100,
+      iterations: 1,
+    }
+  );
+};
+
+form.addEventListener('change', pancakePriceCalc);
